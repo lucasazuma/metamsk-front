49a88bd87a1d38f069f774e226aa444d7b0cde19
lucas <azumalucas@gmail.com>
2022-02-02 10:29:49 -0300

    bin changes

--- src/App.js
+++ src/App.js
@@ -2,39 +2,39 @@
 import './App.css';
 import Button from '@mui/material/Button';
 import detectEthereumProvider from '@metamask/detect-provider';
+import api from "./services/api";
 
 function App() {
-    function startApp(provider) {
 
-        if (provider !== window.ethereum) {
-            console.error('Do you have multiple wallets installed?');
-        } else{
-            console.log('vc é uma maquina de vencer')
-            console.log(provider)
-        }
-    }
-
-    async function batata(){
-        const provider = await detectEthereumProvider();
-
-        if (provider) {
-            console.log('É TETRA PORRA'); // Initialize your app
-            startApp(provider)
-        } else {
-            console.log('SEM MetaMask!');
-        }
+    async function personalSign(account, message) {
+        const signature = await window.ethereum.request({ method: 'personal_sign', params: [ message, account ] });
+        return signature;
     }
 
     async function connectWallet(){
         if (window.ethereum) { //check if Metamask is installed
             try {
-                const address = await window.ethereum.enable(); //connect Metamask
+                const address = await window.ethereum.request({ method: 'eth_requestAccounts' }); //connect Metamask
+                let user = {}
                 const obj = {
                     connectedStatus: true,
                     status: "",
                     address: address
                 }
-                console.log(obj)
+                await api
+                    .get(`/api/v1/meta_login/${obj.address}`)
+                    .then((response) => (user = response.data))
+                    .catch((err) => {
+                        console.error("ops! ocorreu um erro" + err);
+                    });
+                if (user.data.nonce){
+                    const requestTime = new Date().getTime();
+                    const signature = await personalSign(obj.address[0], user.data.nonce)
+
+
+                }else{
+                    alert('assina ai pai')
+                }
                 return obj;
 
             } catch (error) {
